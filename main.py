9c5ab177cc376b77e18132a5111948b0028092d6
neyen <neyen.gmbh@gmail.com>
2023-04-29 01:04:51 +0200

    29,04,2023

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
 
-year = input('Which year would you like to travel to? Type it in YYYY format: ')
+
+'''year = input('Which year would you like to travel to? Type it in YYYY format: ')
 month = input('Which month would you like to travel to? Type it in MM format: ')
-day = input('Which day would you like to travel to? Type it in DD format: ')
+day = input('Which day would you like to travel to? Type it in DD format: ')'''
+CLIENT_ID = "447643cef8ea4bf38686fbebfd481a06"
+CLIENT_SECRET = "9831d46cd37e4486a00082e2451b557c"
+SPOTIPY_REDORECT ="http://localhost:8888/callback"
 
-url = f'https://www.billboard.com/charts/hot-100/{year}-{month}-{day}/'
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri=SPOTIPY_REDORECT,
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
+
+'''url = f'https://www.billboard.com/charts/hot-100/{year}-{month}-{day}/'
 
 response = requests.get(url)
 billboard = response.text
@@ -18,4 +38,4 @@ print()
 
 song_name = [song.getText(strip=True) for song in all_song]
 
-print(song_name)
+print(song_name)'''
